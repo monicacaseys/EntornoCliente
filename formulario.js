e811656d7325ac1932955c1b67ef6ad73cedf021
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-17 12:55:33 +0200

    78

--- formulario.js
+++ formulario.js
@@ -1,34 +1,23 @@
-function calcularResultado() {
+'use strict';
 
-
-    var numero1 = parseFloat(document.getElementById("numero1").value);
-    var numero2 = parseFloat(document.getElementById("numero2").value);
-    var operacion = document.getElementById("operacion").value;
-
-
-    var resultado;
  
-    if (operacion === "sumar") {
-        resultado = numero1 + numero2;
-      } else if (operacion === "restar") {
-        resultado = numero1 - numero2;
-      } else if (operacion === "multiplicar") {
-        resultado = numero1 * numero2;
-      } else if (operacion === "dividir") {
-        if (numero2 === 0) {
-          resultado = "No se puede dividir por cero.";
-        } else {
-          resultado = numero1 / numero2;
+  function calcularDNI() {
+    var letras2 = 'TRWAGMYFPDXBNJZSQVHLCKET';
+    var dni=document.getElementById('dni').value;
+    var letra=document.getElementById('letra').value;
+    var resultado=document.getElementById('resultado');
+    
+    if (dni.length === 8){
+        var resto= dni%23;
+        var encontrado = letras2.charAt(resto);
+
+        if (letra.toUpperCase()===encontrado){
+        resultado.textContent="el dni es valido";
+        } else{
+            resultado.textContent="el dni no es valido";
+      
         }
-      } else {
-        resultado = "Operación no válida";
-      }
+    }
+    return false;
  
-    // Mostrar el resultado en el elemento con id "resultado"
-    document.getElementById("resultado").textContent = "Resultado: " + resultado;
-    console.log("Resultado: " + resultado);
-    console.log("num1: " + numero1);
-    console.log("num2: " + numero2);
-
   }
- 
--- sumar.js
+++ sumar.js
@@ -8,7 +8,39 @@ function nombre(nombre){
     console.log(nombre);
 }
  nombre ("monica");
- 
+
 function sumarParm (a,b){
     console.log(a+b);
 }
+/* function calcularResultado() {
+
+
+    var numero1 = parseFloat(document.getElementById("numero1").value);
+    var numero2 = parseFloat(document.getElementById("numero2").value);
+    var operacion = document.getElementById("operacion").value;
+    var resultado;
+ 
+    if (operacion === "sumar") {
+        resultado = numero1 + numero2;
+      } else if (operacion === "restar") {
+        resultado = numero1 - numero2;
+      } else if (operacion === "multiplicar") {
+        resultado = numero1 * numero2;
+      } else if (operacion === "dividir") {
+        if (numero2 === 0) {
+          resultado = "No se puede dividir por cero.";
+        } else {
+          resultado = numero1 / numero2;
+        }
+      } else {
+        resultado = "Operación no válida";
+      }
+ 
+    // Mostrar el resultado en el elemento con id "resultado"
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
+    console.log("Resultado: " + resultado);
+    console.log("num1: " + numero1);
+    console.log("num2: " + numero2);
+    console.log("operacion: " + numero2);
+
+  } */
