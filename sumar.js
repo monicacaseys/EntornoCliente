acf196182888bb81a02eaf53eff1756f3e4732ae
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-17 09:23:27 +0200

    df

---
+++ sumar.js
@@ -0,0 +1,14 @@
+function sumar ( a,b){
+    return (a+b);
+}
+sumar (1,2);
+
+
+function nombre(nombre){
+    console.log(nombre);
+}
+ nombre ("monica");
+ 
+function sumarParm (a,b){
+    console.log(a+b);
+}
