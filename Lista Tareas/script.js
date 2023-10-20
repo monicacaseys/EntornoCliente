3555bc4701eb6b43c07f76f74d773b848dadf987
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-20 08:51:20 +0200

    Update script.js

--- Lista Tareas/script.js
+++ Lista Tareas/script.js
@@ -1,23 +1,23 @@
 "use strict";
 
-var tareaForm=document.getElementById("tareaForm");
-var nuevaTarea=document.getElementById("nuevaTarea");
-var listaTareas=document.getElementById("listaTareas");
+var tareaForm = document.getElementById("tareaForm");
+var nuevaTarea = document.getElementById("nuevaTarea");
+var listaTareas = document.getElementById("listaTareas");
 
-tareaForm.addEventListener("submit",function(event){
-event.preventDefault(); 
+tareaForm.addEventListener("submit", function (event) {
+    event.preventDefault();
 
-var tareaTexto =nuevaTarea.value.trim();
-if(tareaTexto!==""){
-    agregarTarea(tareaTexto);
-}
+    var tareaTexto = nuevaTarea.value.trim();
+    if (tareaTexto !== "") {
+        agregarTarea(tareaTexto);
+    }
 
 
 });
 
-function agregarTarea(texto){
-    var tareaItem=document.createElement("li");
-    tareaItem.innerHTML=`
+function agregarTarea(texto) {
+    var tareaItem = document.createElement("li");
+    tareaItem.innerHTML = `
     <span>${texto}</span>
     <button class="btn btn-danger btn-sm float-right" onclick="eliminarTarea(this)">Eliminar</button>
     <button class="btn btn-sucess btn-sm float-right" onclick="modificarTarea(this)">Modificar</button>`;
@@ -25,39 +25,19 @@ function agregarTarea(texto){
     listaTareas.appendChild(tareaItem);
 }
 
-function eliminarTarea(boton){
+function eliminarTarea(boton) {
 
-    var tareaItem= boton.parentElement;
+    var tareaItem = boton.parentElement;
     listaTareas.removeChild(tareaItem);
 
 }
 
-function modificarTarea(element) {
-    var tareaItem = element.parentElement;
-    var tareaSpan = tareaItem.querySelector("span");
+function modificarTarea(boton) {
+    var tareaItem = boton.parentElement; //cogemos el elemento padre
+    var tareaTexto = tareaItem.querySelector("span").textContent; //te coge el elemento a cambiar
+    var nuevoTexto = prompt("Modificar tarea: ", tareaTexto);
+    if (nuevoTexto !== null && nuevoTexto!=="") { //pongo as dos por si acaso
+        tareaItem.querySelector("span").textContent = nuevoTexto;
+    }
 
-    // Crear un campo de entrada para la edición
-    var inputEdicion = document.createElement("input");
-    inputEdicion.type = "text";
-    inputEdicion.value = tareaSpan.textContent;
-
-    // Reemplazar el texto estático con el campo de entrada
-    tareaItem.replaceChild(inputEdicion, tareaSpan);
-
-    // Crear un botón "Guardar" para confirmar la edición
-    var botonGuardar = document.createElement("button");
-    botonGuardar.className = "btn btn-success btn-sm float-right";
-    botonGuardar.textContent = "Guardar";
-
-    // Manejar el evento click del botón "Guardar"
-    botonGuardar.addEventListener("click", function () {
-        // Actualizar el contenido del elemento span con el nuevo valor del campo de entrada
-        tareaSpan.textContent = inputEdicion.value;
-
-        // Remover el campo de entrada y el botón "Guardar"
-        tareaItem.removeChild(inputEdicion);
-        tareaItem.removeChild(botonGuardar);
-    });
-
-    tareaItem.appendChild(botonGuardar);
 }
