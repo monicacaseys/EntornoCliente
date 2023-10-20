7a5fc52f044ec59dac7011a5fee8533ac94b8e3f
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-20 11:18:26 +0200

    l

---
+++ JuegoPPT/juego.js
@@ -0,0 +1,28 @@
+"use strict";
+function jugar (eleccionUsuario){
+
+    //eleccion makina
+    var opciones=["piedra","papel","tijera"];
+     
+    var eleccionMakina= opciones[Math.floor(Math.random()*3)];
+
+    //Decir quien gana
+    var ganador=kienGana(eleccionUsuario,eleccionMakina);
+    document.getElementById('ganador').innerHTML = ganador;
+
+    
+
+}
+
+function kienGana(eleccionUsuario,eleccionMakina){
+    if (eleccionUsuario===eleccionMakina){
+        return "Es un empate";
+    }
+    if(eleccionUsuario==='piedra' && eleccionMakina==='tijera' 
+    ||eleccionUsuario==='papel' && eleccionMakina==='piedra' 
+    || eleccionUsuario==='tijera' && eleccionMakina==='papel'){
+        return "Has ganado!"
+    }else{
+        return "Has perdido :(. Gana la makina"
+    }
+}
