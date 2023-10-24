3a6beae9873b20c8156d3ade5112214b31b5a486
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2023-10-24 09:14:39 +0200

    i

---
+++ numOrdenados.js
@@ -0,0 +1,7 @@
+'use strict';
+var arrayNumbers = [10, 8, 9, 5, 3, 78, 23];
+var orden = arrayNumbers.sort((a,b)=>{
+    return b-a;
+ }); 
+console.log(orden);
+// solit sort join: ejercicio pedir numeros por consola y despues ordenalos
