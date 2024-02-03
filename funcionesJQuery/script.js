716387a9cb98822e14ccdc17e4582fd412f31b92
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2024-02-03 13:45:36 +0100

    ,

--- funcionesJQuery/script.js
+++ funcionesJQuery/script.js
@@ -1,5 +1,15 @@
 "use strict";
 
 $(document).ready(function(){
-    $('.item').not('.special').hide();
+    // Selecciona todos los elementos .item que no tienen la clase .special y ocúltalos.
+  //  $('.item').not('.special').hide();
+
+    // Selecciona todos los elementos .special y cambia su fondo a verde.
+    $('.special').css('background-color', 'green');
+
+    // Selecciona el segundo elemento .item y añade la clase .highlight.
+    $('.item:eq(1)').addClass('highlight');
+
+    // Selecciona los elementos .item del índice 2 al 4 y cambia su borde a rojo.
+    $('.item:gt(1):lt(4)').css('border','1px solid red');
 })
