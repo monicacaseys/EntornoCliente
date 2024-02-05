1d815a66af6839489c7128bc3cb217d91e93d4a9
monicacaseys <115183037+monicacaseys@users.noreply.github.com>
2024-02-05 08:04:16 +0100

    BM

---
+++ J/contadorr/src/app/contador/contador.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contador',
+  templateUrl: './contador.component.html',
+  styleUrl: './contador.component.css'
+})
+export class ContadorComponent {
+  contador: number=0;
+
+  incrementar(){
+    this.contador++;
+  }
+
+  decrementar(){
+    this.contador--;
+  }
+}
